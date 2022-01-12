a3e69d23c77f14804f9791a5de2254a4401990b6
VAxRAxD <varadp763@gmail.com>
2022-01-12 10:48:38 +0530

    Updated Insertion Sort

--- Algorithms/insertionsort.py
+++ Algorithms/insertionsort.py
@@ -1,3 +1,4 @@
+#Approach 1
 arr=[40,10,50,30,20]
 for i in range(1,len(arr)):
 	while i>0:
@@ -6,4 +7,16 @@ for i in range(1,len(arr)):
 		else:
 			break
 		i-=1
+print(*arr)
+
+
+#Approach 2
+arr=[40,10,50,30,20]
+for i in range(1,len(arr)):
+	key=arr[i]
+	j=i-1
+	while j>=0 and arr[j]>key:
+		arr[j+1]=arr[j]
+		j-=1
+	arr[j+1]=key
 print(*arr)
