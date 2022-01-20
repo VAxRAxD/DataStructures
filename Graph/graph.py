e2a9d96f5156a2c01fe00fce83de0644f501b591
VAxRAxD <varadp763@gmail.com>
2022-01-20 19:04:25 +0530

    Updated Graphs

--- Graph/graph.py
+++ Graph/graph.py
@@ -1,34 +1,36 @@
 from collections import deque
 class Graph:
-	def __init__(self,nodes,start,adj):
+	def __init__(self,nodes,adj):
 		self.nodes=nodes
-		self.start=start
 		self.adj=adj
 
 	def bfs(self):
 		visited=[0]*self.nodes
 		buffer=deque()
-		buffer.appendleft(self.start)
 		bfs=[]
-		while len(buffer)!=0:
-			node=buffer.pop()
-			if visited[node-1]==0:
-				buffer.extendleft(self.adj[node-1])
-				bfs.append(node)
-				visited[node-1]=1
-			else:
-				continue
+		for i in range(1,self.nodes+1):
+			if visited[i-1]==0:
+				buffer.appendleft(i)
+				while len(buffer)!=0:
+					node=buffer.pop()
+					if visited[node-1]==0:
+						buffer.extendleft(self.adj[node-1])
+						bfs.append(node)
+						visited[node-1]=1
 		return " ".join(str(elem) for elem in bfs)
+
 
 	def dfs(self):
 		dfs=[]
 		visited=[0]*self.nodes
 		buffer=deque()
-		buffer.appendleft(self.start)
-		while len(buffer)!=0:
-			curr=buffer.pop()
-			if visited[curr-1]==0:
-				self.traversal(curr-1,visited,self.adj,dfs,buffer)
+		for i in range(1,self.nodes+1):
+			if visited[i-1]==0:
+				buffer.appendleft(i)
+				while len(buffer)!=0:
+					curr=buffer.pop()
+					if visited[curr-1]==0:
+						self.traversal(curr-1,visited,self.adj,dfs,buffer)
 		return " ".join(str(elem) for elem in dfs)
 
 	def traversal(self,node,visited,adj,dfs,buffer):
@@ -38,14 +40,36 @@ class Graph:
 			buffer.appendleft(nodes)
 			if visited[nodes-1]==0:
 				self.traversal(nodes-1,visited,self.adj,dfs,buffer)
-n=5
-m=[[1,2],[2,3],[2,4],[3,5],[4,5]]
-start=5
+
+
+	def cycleBfs(self):
+		visited=[0]*self.nodes
+		buffer=deque()
+		bfs=[]
+		cycle=False
+		for i in range(1,self.nodes+1):
+			if visited[i-1]==0:
+				buffer.appendleft([i,-1])
+				while len(buffer)!=0:
+					node,origin=buffer.pop()
+					visited[node-1]=1
+					for nodes in self.adj[node-1]:
+						if visited[nodes-1]==1:
+							if nodes!=origin:
+								cycle=True
+						else:
+							buffer.appendleft([nodes,node])
+							visited[nodes-1]=1
+		return cycle
+n=11
+m=[[1,2],[2,4],[3,5],[5,10],[5,6],[6,7],[7,8],[10,9],[9,8],[8,11]]
 adj=[[] for _ in range(n)]
 for edge in m:
 	i,j=edge[0],edge[1]
 	adj[i-1].append(j)
 	adj[j-1].append(i)
-g=Graph(n,start,adj)
+print(adj)
+g=Graph(n,adj)
 print(g.bfs())
 print(g.dfs())
+print(g.cycleBfs())
