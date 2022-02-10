2f19dc0d3516cc3d2b2f209fc061cedf3ebe34fb
Varad Prabhu <varadp763@gmail.com>
2022-02-10 11:34:53 +0530

    Added Toposort DFS

--- Graph/graph.py
+++ Graph/graph.py
@@ -20,28 +20,40 @@ class Graph:
 		return " ".join(str(elem) for elem in bfs)
 
 
-	def dfs(self):
+	def dfs(self,func=None):
 		dfs=[]
 		visited=[0]*self.nodes
 		buffer=deque()
-		for i in range(1,self.nodes+1):
-			if visited[i-1]==0:
-				buffer.appendleft(i)
-				while len(buffer)!=0:
-					curr=buffer.pop()
-					if visited[curr-1]==0:
-						self.traversal(curr-1,visited,self.adj,dfs,buffer)
+		for i in range(self.nodes):
+			buffer.appendleft(i)
+			while len(buffer)!=0:
+				curr=buffer.pop()
+				if visited[curr]==0:
+					if func=="Toposort":
+						self.sorting(curr,visited,self.adj,dfs,buffer)
+					else:
+						self.traversal(curr,visited,self.adj,dfs,buffer)
 		return " ".join(str(elem) for elem in dfs)
 
 	def traversal(self,node,visited,adj,dfs,buffer):
-		dfs.append(node+1)
+		dfs.append(node)
 		visited[node]=1
 		for nodes in adj[node]:
 			buffer.appendleft(nodes)
-			if visited[nodes-1]==0:
-				self.traversal(nodes-1,visited,self.adj,dfs,buffer)
-
-
+			if visited[nodes]==0:
+				self.traversal(nodes,visited,self.adj,dfs,buffer)
+	
+	def toposortDfs(self):
+		return self.dfs("Toposort")
+	
+	def sorting(self,node,visited,adj,dfs,buffer):
+		visited[node]=1
+		for nodes in adj[node]:
+			buffer.appendleft(nodes)
+			if visited[nodes]==0:
+				self.traversal(nodes,visited,self.adj,dfs,buffer)
+		dfs.append(node)
+		
 	def cycleBfs(self):
 		visited=[0]*self.nodes
 		buffer=deque()
@@ -61,15 +73,14 @@ class Graph:
 							buffer.appendleft([nodes,node])
 							visited[nodes-1]=1
 		return cycle
-n=11
-m=[[1,2],[2,4],[3,5],[5,10],[5,6],[6,7],[7,8],[10,9],[9,8],[8,11]]
+n=6
+m=[[2,3],[3,1],[4,0],[4,1],[5,0],[5,2]]
 adj=[[] for _ in range(n)]
 for edge in m:
 	i,j=edge[0],edge[1]
-	adj[i-1].append(j)
-	adj[j-1].append(i)
-print(adj)
+	adj[i].append(j)
 g=Graph(n,adj)
-print(g.bfs())
+# print(g.bfs())
 print(g.dfs())
-print(g.cycleBfs())
+print(g.toposortDfs())
+# print(g.cycleBfs())
