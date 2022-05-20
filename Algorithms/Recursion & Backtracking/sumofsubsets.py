b1feddb40ddb64ee6b6c789592330bb4ac83725b
Varad Prabhu <varadp763@gmail.com>
2022-05-20 10:29:28 +0530

    Added Algorithms

--- Algorithms/Recursion & Backtracking/sumofsubsets.py
+++ Algorithms/Recursion & Backtracking/sumofsubsets.py
@@ -1,3 +1,26 @@
+"""
+ALGORITHM_SUM_OF_SUBSETS(s,r,k,m)
+#arr[1:n] contains the set of numbers
+#m is the maximum sun
+#x{1..n} is the solution vector
+
+s=0 #To store the sum obtained
+k=0
+r=SUM(arr) #To store the remaining weight
+if s==m then
+    PRINT x
+if k < n then
+    / Left Tree /
+    if s+arr[k]<=m then
+        x[k]=1
+        ALGORITHM_SUM_OF_SUBSETS(s+arr[k],r-arr[k],k+1,m)
+    / Right Tree /
+    x[k]=0
+    if s+arr[k+1]<=m and s+r-arr[k]>=m then
+        ALGORITHM_SUM_OF_SUBSETS(s,r-arr[k],k+1,m)
+
+"""
+
 arr=list(map(int,input("Enter the numbers : ").split()))
 m=int(input("Enter the maximum sum : "))
 x=[0 for _ in range(len(arr))]
